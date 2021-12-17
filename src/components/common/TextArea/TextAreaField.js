fd40c4d85281ac1de8750511bd7a359caf4aa0f8
DimitarGrozev5 <dimgrozev5@gmail.com>
2021-12-17 17:58:48 +0200

    Update TextAreaField.js

--- src/components/common/TextArea/TextAreaField.js
+++ src/components/common/TextArea/TextAreaField.js
@@ -1,17 +1,27 @@
-import { useContext } from "react";
+import { useContext, useState, useRef } from "react";
 import SystemsContext from "../../../store/systems-context";
 import classes from "./TextAreaField.module.css";
 
 const TextAreaField = (props) => {
   const ctx = useContext(SystemsContext);
 
+  const inputFieldRef = useRef();
+
+  const [isInput, setIsInput] = useState(false);
+  const clickHandler = (event) => {
+    setIsInput(true);
+    inputFieldRef.current.click();
+  }
+
   return (
     <div
       className={`${classes.field} ${props.header ? classes.header : ""} ${
         classes[ctx.selectedInputCS]
       } ${classes[ctx.selectedInputHS]}`}
+      onClick={clickHandler}
     >
-      {props.value}
+      {!isInput && props.value}
+      {isInput && <input type="text" ref={inputFieldRef} defaultValue={props.value} />}
     </div>
   );
 };
