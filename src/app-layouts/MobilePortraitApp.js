521beb50c0770077197da26b8cf4c5264588a250
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-01-10 12:15:33 +0200

    Tab 1 and Tab2 redesigned

--- src/app-layouts/MobilePortraitApp.js
+++ src/app-layouts/MobilePortraitApp.js
@@ -6,7 +6,7 @@ import classes from "./MobilePortraitApp.module.css";
 import { useState } from "react";
 
 const MobilePortraitApp = (props) => {
-  const [activePage, setActivePage] = useState("3");
+  const [activePage, setActivePage] = useState("1");
 
   const changeActivePageHandler = (target) => {
     setActivePage(target);
