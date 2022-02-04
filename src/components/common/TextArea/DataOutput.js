2cc4a756e8ffb268b22876a33f5d40a624de081c
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-02-04 17:14:35 +0200

    production build beta

--- src/components/common/TextArea/DataOutput.js
+++ src/components/common/TextArea/DataOutput.js
@@ -10,8 +10,56 @@ const DataOutput = () => {
     output = "No data to show";
   }
 
+  // If there was an error
   if (transformedData && transformedData.error) {
     output = transformedData.error;
+  }
+
+  if (transformedData && !transformedData.error) {
+    output = (
+      <table>
+        <thead>
+          <tr>
+            <td>№</td>
+            <td>X</td>
+            <td>Y</td>
+            <td>H</td>
+            <td>Code</td>
+          </tr>
+        </thead>
+        <tbody>
+          {transformedData.map((row) => {
+            const fields = [];
+            if (row["#"]) {
+              fields.push(row["#"]);
+            }
+            if (row["X"]) {
+              fields.push(+row["X"].toFixed(3));
+            }
+            if (row["Y"]) {
+              fields.push(+row["Y"].toFixed(3));
+            }
+            if (row["H"]) {
+              fields.push(+row["H"].toFixed(3));
+            }
+            if (row["Code"]) {
+              fields.push(row["Code"]);
+            }
+            if (row["var"]) {
+              fields.push(...row["var"]);
+            }
+
+            return (
+              <tr>
+                {fields.map((field) => (
+                  <td>{field}</td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
   }
 
   return <div>{output}</div>;
--- src/store/thunks/transform-input-data.js
+++ src/store/thunks/transform-input-data.js
@@ -30,12 +30,22 @@ const transformInputDataThunk = () => (dispatch, getState) => {
   // If the input data is not transformed get the input data
   const rawInputData = getState().inputData.rows;
 
+  // If the input text is empty skip
+  if (
+    rawInputData.length === 1 &&
+    rawInputData[0].fields.length === 1 &&
+    rawInputData[0].fields[0].value === ""
+  ) {
+    dispatch(systemsActions.setTransformedData(null));
+    return;
+  }
+
   // Reformat the data to be sendable to the server
   const reformatedData = rawInputData.reduce((output, row) => {
     const headers = ["#", "X", "Y", "H", "Code"];
 
     const rowContents = row.fields.reduce((dataPoint, field, i) => {
-      const key = headers.length ? headers.shift() : `var${i - 5}`;
+      const key = headers.length ? headers.shift() : `var${i}`;
       return { ...dataPoint, [key]: field.value };
     }, {});
 
@@ -47,9 +57,62 @@ const transformInputDataThunk = () => (dispatch, getState) => {
     data: reformatedData,
   };
 
-  console.log(postData)
+  // console.log(postData);
 
+  dispatch(
+    systemsActions.setTransformedData({
+      error: "Loading...",
+    })
+  );
   // Fetch data
+  // fetch("http://127.0.0.1/online-trans-api/transform.php", {
+  fetch("online-trans-api/transform.php", {
+    method: "POST",
+    mode: "cors",
+    body: JSON.stringify(postData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      // res
+      //   .clone()
+      //   .text()
+      //   .then((t) => console.log(t));
+      if (res.ok) {
+        return res.json();
+      } else {
+        return res.json().then((data) => {
+          throw new Error(data);
+        });
+      }
+    })
+    .then((data) => {
+      if (data.error) {
+        throw new Error(data.error);
+      } else {
+        return data;
+      }
+    })
+    .then((data) => {
+      if (data.error) {
+        dispatch(
+          systemsActions.setTransformedData({
+            error: data.error,
+          })
+        );
+      } else {
+        const count = Object.values(data).length;
+
+        const dataArr = [];
+        for (let i = 1; i <= count; i++) {
+          dataArr.push(data[i.toString()]);
+        }
+
+        dispatch(systemsActions.setTransformedData(dataArr));
+      }
+    })
+    .catch((err) => console.log(err));
 };
 
 export default transformInputDataThunk;
