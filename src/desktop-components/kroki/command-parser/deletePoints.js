3538cd0f7b57378f70b572d4654b3f5e7b206ad7
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-04-16 19:47:39 +0300

    format

--- src/desktop-components/kroki/command-parser/deletePoints.js
+++ src/desktop-components/kroki/command-parser/deletePoints.js
@@ -1,6 +1,6 @@
 import P from "parsimmon";
 import { deletePointCommand } from "./command-creators";
-/////////////////////// All commands
+
 //// Command that deletes a point
 /// Syntax:
 /// .del
@@ -57,27 +57,32 @@ const delNearestHandler = (parser, pts) => (reduced, pt, index) => {
 
 // Actions reducer
 const createActions = (pts) => (reduced, pt, index) => {
+  // Init handlers
   const delPrevHandler_ = delPrevHandler(delPrevParser);
   const delNearestHandler_ = delNearestHandler(delNearestParser, pts);
 
+  // If hr is not null, then one parser managed to detect a valid code
   let hr = pipeParsers(delPrevHandler_, delNearestHandler_)(reduced, pt, index);
-
   if (hr) {
     return createReduced(hr.pointer + 1, hr.actions);
   }
 
+  // If hr was null then return the current actions and update the pointer
   return createReduced(index + 1, reduced.actions);
 };
 const createReduced = (pointer, actions) => ({ pointer, actions });
 const initReduced = createReduced(0, []);
 
+// Execute command to generate actions
 export const deletePoints = (points) => {
   const createActions_ = createActions(points);
   const reduced = points.reduce(createActions_, initReduced);
   return reduced.actions;
 };
 
-// Helper functions
+/// Helper functions
+
+// Find the nearest point
 function nearest(points, point) {
   const dist = (pt1, pt2) =>
     Math.sqrt((pt1.x - pt2.x) ** 2 + (pt1.y - pt2.y) ** 2);
@@ -94,6 +99,8 @@ function nearest(points, point) {
   return points.reduce(compare, [null, 1000000]);
 }
 
+// Pipe value true the provided handlers
+// If one handler returns a valid value, then skip all of the rest
 function pipeParsers(...handlers) {
   return (reduced, pt, index) =>
     handlers.reduce((prev, curr) => {
