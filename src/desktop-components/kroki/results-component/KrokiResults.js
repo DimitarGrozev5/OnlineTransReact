e19316581fde3aa7d74c4d7c8ba1a9d5e5b8d23b
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-05-05 15:33:13 +0300

    rudimentary tabs work

--- src/desktop-components/kroki/results-component/KrokiResults.js
+++ src/desktop-components/kroki/results-component/KrokiResults.js
@@ -1,7 +1,36 @@
+import { useState } from "react";
 import styles from "./KrokiResults.module.css";
 
 const KrokiResults = ({ lines }) => {
-  return <div>{lines.toString()}</div>;
+  const tabs = [
+    {
+      title: "Линии",
+      content: lines.toString(),
+    },
+    {
+      title: "test",
+      content: "test",
+    },
+  ];
+
+  const [activeTab, setActiveTab] = useState(0);
+
+  const changeTabHandler = (index) => () => setActiveTab(index);
+
+  return (
+    <>
+      <nav>
+        <ul>
+          {tabs.map((tab, index) => (
+            <li className={index === activeTab ? styles["active-tab"] : ""}>
+              <button onClick={changeTabHandler(index)}>{tab.title}</button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div>{tabs[activeTab].content}</div>
+    </>
+  );
 };
 
 export default KrokiResults;
