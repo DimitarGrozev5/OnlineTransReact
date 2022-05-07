c87df3a197f7522c451acbf5eda9d9691f54908f
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-05-07 19:12:48 +0300

    dxf message is updated

--- src/components/common/TextArea/DxfOverview.js
+++ src/components/common/TextArea/DxfOverview.js
@@ -1,7 +1,12 @@
 import styles from "./DxfOverview.module.css";
 
 const DxfOverview = (props) => {
-  return <div>DXF is loaded</div>;
+  return (
+    <>
+      <div>DXF is loaded</div>
+      <div>Currently only LWPolylines are supported</div>
+    </>
+  );
 };
 
 export default DxfOverview;
