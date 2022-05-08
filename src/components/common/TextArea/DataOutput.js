106517a04df32454af5d8256dd71af3541cd28d1
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-05-08 15:53:43 +0300

    dxf succesfuly transformed and saved

--- src/components/common/TextArea/DataOutput.js
+++ src/components/common/TextArea/DataOutput.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import DataOutputControls from "./DataOutputControls";
 import TextArea from "./TextArea";
@@ -8,6 +8,9 @@ import TextAreaWraper from "./TextAreaWraper";
 const DataOutput = () => {
   const transformedData = useSelector((state) => state.systems.transformedData);
   const selected = useSelector((state) => state.systems.selectedSystems.output);
+
+  const dxfData = useSelector((state) => state.inputData.dxfData);
+  const saveHref = useRef();
 
   let output = transformedData;
 
@@ -27,7 +30,7 @@ const DataOutput = () => {
     setWrap((prevState) => !prevState);
   };
 
-  if (transformedData && !transformedData.error) {
+  if (transformedData && !transformedData.error && !dxfData) {
     output = (
       <React.Fragment>
         <DataOutputControls
@@ -55,6 +58,65 @@ const DataOutput = () => {
           </table>
         </TextAreaWraper>
       </React.Fragment>
+    );
+  }
+
+  // If there is a dxf file loaded
+  if (transformedData && !transformedData.error && dxfData) {
+    // Map transformed data to dxfData Points Map
+    const tranformedEntityPoints = dxfData.entityPointsMap.map((dxfPt, i) => {
+      const trPt = {};
+      if ("xLineIndex" in dxfPt) {
+        trPt.xLineIndex = dxfPt.xLineIndex;
+      }
+      if ("zLineIndex" in dxfPt) {
+        trPt.zLineIndex = dxfPt.zLineIndex;
+      }
+      trPt.x = transformedData[i][2];
+      trPt.y = transformedData[i][1];
+      if ("h" in dxfPt) {
+        trPt.h = transformedData[3].H;
+      }
+      return trPt;
+    });
+
+    const trDxfFileArr = [...dxfData.dxfFileArr];
+    tranformedEntityPoints.forEach((pt) => {
+      if ("xLineIndex" in pt) {
+        trDxfFileArr[pt.xLineIndex] = pt.x;
+        trDxfFileArr[pt.xLineIndex + 2] = pt.y;
+        if ("h" in pt) {
+          trDxfFileArr[pt.xLineIndex + 4] = pt.h;
+        }
+      }
+      if ("zLineIndex" in pt) {
+        trDxfFileArr[pt.zLineIndex] = pt.h;
+      }
+    });
+
+    const saveToFile = () => {
+      const data = trDxfFileArr.join("\n");
+      const blob = new Blob([data], {
+        type: "text/plain;charset=utf-8",
+      });
+      const url = URL.createObjectURL(blob);
+      saveHref.current.href = url;
+      saveHref.current.download = `tr.dxf`;
+      saveHref.current.click();
+    };
+
+    output = (
+      <>
+        <button onClick={saveToFile}>Save DXF file</button>
+        <a
+          href="/"
+          ref={saveHref}
+          style={{ display: "none" }}
+          // className={style["save-link"]}
+        >
+          save
+        </a>
+      </>
     );
   }
 
