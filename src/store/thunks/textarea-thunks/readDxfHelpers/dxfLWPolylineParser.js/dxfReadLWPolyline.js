7b6ea7cd7f539d197c6e479a0e00d18b7149dab0
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-05-08 18:45:19 +0300

    cleanup

--- src/store/thunks/textarea-thunks/readDxfHelpers/dxfLWPolylineParser.js/dxfReadLWPolyline.js
+++ src/store/thunks/textarea-thunks/readDxfHelpers/dxfLWPolylineParser.js/dxfReadLWPolyline.js
@@ -1,9 +1,7 @@
-import { readVertexes } from "./dxfReadVertexes";
 import { readLineElevation } from "./dxfReadLineElevation";
 import { read2DVertecies } from "../common-parsers/dxfReadAll2DVertecies";
 
 export const readLWPolyline = (dxfLines, pointer) => {
-  // const allPoints = readVertexes(dxfLines, pointer);
   const allPoints = read2DVertecies(dxfLines, pointer);
 
   const firstPt = allPoints.length ? allPoints[0] : null;
