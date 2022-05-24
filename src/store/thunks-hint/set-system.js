6cfd400ed9458043c3cc5f305c4a4663edf0601a
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-05-24 18:43:57 +0300

    comment for hints

--- src/store/thunks-hint/set-system.js
+++ src/store/thunks-hint/set-system.js
@@ -3,6 +3,11 @@ import { systemsActions } from "../systemsSlice";
 import hintInputsThunk from "./hint-inputs";
 import hintOutputsThunk from "./hint-outputs";
 
+// Important!!!
+// In the first version once the systems are set, the hints stop showing
+// Currently I have disabled that and the hints will show indefinetly
+// To return to the first version uncomment the lines below
+
 const setSystemThunk = (data) => (dispatch, getState) => {
   const hints = getState().hints;
 
